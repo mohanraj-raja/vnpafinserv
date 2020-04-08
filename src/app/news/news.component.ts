ad5b07a4a304d3b94737fe0942d94266de7283a6
mohanraj-raja <mohanraj.raja.tech@gmail.com>
2020-04-08 18:51:30 +0200

    added postedOn

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -8,9 +8,9 @@ import { News } from '../model/news';
 })
 export class NewsComponent implements OnInit {
    private newses:News[] =[
-    {title:'HDFC goes High',description:"HDFC goes to 8%",publishedOn:'8 APR 2020'},
-      {title:'HDFC goes low',description:"HDFC goes to 7%",publishedOn:'8 APR 2020'},
-        {title:'HDFC goes high',description:"HDFC goes to 6%",publishedOn:'8 APR 2020'}
+    {title:'HDFC goes High',description:"HDFC goes to 8%",postedOn:'8 APR 2020'},
+      {title:'HDFC goes low',description:"HDFC goes to 7%",postedOn:'8 APR 2020'},
+        {title:'HDFC goes high',description:"HDFC goes to 6%",postedOn:'8 APR 2020'}
 
   ];
 
