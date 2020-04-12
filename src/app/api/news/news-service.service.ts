ac558aa1d44eed550dea6c8ae2012fb92b5d0edc
mohanraj-raja <mohanraj.raja.tech@gmail.com>
2020-04-12 15:26:33 +0200

    modified model class

--- src/app/api/news/news-service.service.ts
+++ src/app/api/news/news-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { map } from "rxjs/Operators";
+import { map } from "rxjs/operators";
 import { Observable } from "rxjs";
 import {INewsinterface} from './newsinterface';
 
@@ -10,6 +10,16 @@ import {INewsinterface} from './newsinterface';
 @Injectable()
 export class NewsServiceService {
 
-  constructor() { }
+  constructor(public http: HttpClient) {  }
+
+   getNews(): Observable<INewsinterface> {
+    return this.http
+      .get("https://vnpafinservrestapi--mohanrajraja.repl.co/getNews", { params: {} })
+      .pipe(
+        map(response => {
+          return response as INewsinterface;
+        })
+      );
+  }
 
 }
